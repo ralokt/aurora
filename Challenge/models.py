4ec0d9269ea4ab025be80e8e405a09e90d1f7f46
Rene Koller <r.koller@me.com>
2017-06-27 10:24:39 +0200

    disable memoize, did not help

--- Challenge/models.py
+++ Challenge/models.py
@@ -10,7 +10,7 @@ from Stack.models import StackChallengeRelation
 from Review.models import Review
 from Elaboration.models import Elaboration
 from Course.models import Course, CourseUserRelation
-from memoize import memoize
+# from memoize import memoize
 
 import logging
 logger = logging.getLogger(__name__)
@@ -151,7 +151,7 @@ class Challenge(models.Model):
         )
         return final_challenge_ids
 
-    @memoize(timeout=2)
+    # @memoize(timeout=2)
     def is_final_challenge(self):
         final_challenge = self.get_final_challenge()
 
@@ -160,7 +160,7 @@ class Challenge(models.Model):
 
         return False
 
-    @memoize(timeout=2)
+    # @memoize(timeout=2)
     def get_first_challenge(self):
 
         first = Challenge.objects.raw('''
@@ -177,7 +177,7 @@ class Challenge(models.Model):
         ''', [self.id])
         return first[0]
 
-    @memoize(timeout=2)
+    # @memoize(timeout=2)
     def get_final_challenge(self):
         final = Challenge.objects.raw('''
             WITH RECURSIVE challenge_tree(id, prerequisite_id, depth) AS (
